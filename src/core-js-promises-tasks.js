79b946e5b89990ebc631966b58d377c57bb23e0c
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-05-05 01:52:25 +0300

    task 2

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -38,10 +38,12 @@ getPromise(2);
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return new Promise((resolve) => {
+    source.then(() => resolve('success')).catch(() => resolve('fail'));
+  });
 }
-
+getPromiseResult(Promise.resolve('success'));
 /**
  * Takes an array of promises and returns a promise that resolves with the value of the first successfully resolved promise From the given array.
  * If all promises in the array are rejected, it returns a promise that is rejected.
